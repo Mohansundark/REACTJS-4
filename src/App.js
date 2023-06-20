ffd900ffcc7e4936b185d029a1726a1304adb14f
Mohansundark <mohansundar.k2020@vitstudent.ac.in>
2023-06-20 20:28:28 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Task4 from './components/task4';
 import './components/task4.css';
 
 function App() {
-  const [displayTask, setDisplayTask] = useState(false);
+ 
   
   return (
     <div className="App">
-      <input className="btn1" onClick={()=> setDisplayTask(!displayTask)} type="button" value="Toggle" />
-      {displayTask && <Task4 />}
+     <Task4/>
+     
     </div>
   );
 }
